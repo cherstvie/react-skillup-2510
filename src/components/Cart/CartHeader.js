98ad2686c1aba853efdb43f49cbb85ee56299966
cherstvie <oxymanty@gmail.com>
2022-11-28 11:58:10 +0200

    added names to CartHeader

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { keys } from 'lodash'
+import { productsArray } from 'components/Products/productsArray'
 
 export const CartHeader = ({ productsInCart }) => {
     return (
         <div>
             {keys(productsInCart).map((productId) => (
                 <div key={productId}>
-                    {productId} : {productsInCart[productId]}
+                    {productsArray[productId - 1].name} :{' '}
+                    {productsInCart[productId]}
                 </div>
             ))}
         </div>
